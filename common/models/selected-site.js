930ed27af3426fd630245e7a82444f000c1a546a
Nathan Hill <nathanjameshill@gmail.com>
2017-02-08 22:19:25 +1030

    custom rest stub for adding observations

--- common/models/selected-site.js
+++ common/models/selected-site.js
@@ -2,4 +2,36 @@
 
 module.exports = function(Selectedsite) {
 
+    // Selectedsite.createObservation = function(SelectedSite, cb) {
+    //     loopback.Model.Observation
+    // };
+
+
+    // SelectedSite.remoteMethod(
+    //     'createObservation',
+    //     {
+    //         accpets: {arg: 'SelectedSite', type: 'Object', required: true},
+    //         http: { path: '/createObservation', verb: 'post' },
+    //         returns: { arg: 'SelectedSite', type: 'Object'}
+    //     }
+    // );
+
+    Selectedsite.greet = function(siteId, siteCode, surveyDayId, selectedSite, cb) {
+    
+      cb(null, {"siteId": siteId, "siteCode": siteCode, "surveyDayId": surveyDayId});
+    }
+
+    Selectedsite.remoteMethod('greet', {
+          accepts: {arg: 'siteId', type: 'string'},
+          accepts: {arg: 'siteCode', type: 'string'},
+          accepts: {arg: 'surveyDayId', type: 'string'},
+          returns: {arg: 'selectedSite', type: 'Object'}
+    });
+
 };
+
+
+/**
+ * 
+ * @param {Function(Error)} callback
+ */
